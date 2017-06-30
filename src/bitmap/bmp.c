6b8a7aa27ab9b69e3f48c67005c34a8dde7969e1
shengling <shenglinzh@gmail.com>
2017-06-30 14:30:49 +0800

    删除非法字符

--- src/bitmap/bmp.c
+++ src/bitmap/bmp.c
@@ -12,82 +12,82 @@
 *************************/
 int show_photo(const char *fbp, struct fb_var_screeninfo *scrinfo, const char *bmpname)
 {
-　　if(NULL == fbp || NULL == scrinfo || NULL == bmpname)
-　　　　return -1;
+   if(NULL == fbp || NULL == scrinfo || NULL == bmpname)
+    return -1;
 
-　　int line_x = 0, line_y = 0;
-　　unsigned long tmp = 0;
-　　int xres = scrinfo->xres_virtual;　　　　//屏幕宽（虚拟）
-　　int bits_per_pixel = scrinfo->bits_per_pixel;　　//屏幕位数
-　　BitMapFileHeader FileHead;
-　　BitMapInfoHeader InfoHead;
-　　RgbQuad rgb;　　　　
+  int line_x = 0, line_y = 0;
+  unsigned long tmp = 0;
+  int xres = scrinfo->xres_virtual;    //屏幕宽（虚拟）
+  int bits_per_pixel = scrinfo->bits_per_pixel;  //屏幕位数
+  BitMapFileHeader FileHead;
+  BitMapInfoHeader InfoHead;
+  RgbQuad rgb;    
 
-　　unsigned long location = 0;
+  unsigned long location = 0;
 
-　　//打开.bmp文件
-　　FILE *fb = fopen(bmpname, "rb");
-　　if (fb == NULL)
-　　{
-　　　　printf("fopen bmp error\r\n");
-　　　　return -1;
-　　}
+  //打开.bmp文件
+  FILE *fb = fopen(bmpname, "rb");
+  if (fb == NULL)
+  {
+    printf("fopen bmp error\r\n");
+    return -1;
+  }
 
-　　//读文件信息
-　　if (1 != fread( &FileHead, sizeof(BitMapFileHeader),1, fb))
-　　{
-　　　　printf("read BitMapFileHeader error!\n");
-　　　　fclose(fb);
-　　　　return -1;
-　　}
-　　if (memcmp(FileHead.bfType, "BM", 2) != 0)
-　　{
-　　　　printf("it's not a BMP file\n");
-　　　　fclose(fb);
-　　　　return -1;
-　　}
-　　
-　　//读位图信息
-　　if (1 != fread( (char *)&InfoHead, sizeof(BitMapInfoHeader),1, fb))
-　　{
-　　　　printf("read BitMapInfoHeader error!\n");
-　　　　fclose(fb);
-　　　　return -1;
-　　}
-　　
-　　//跳转至数据区
-　　fseek(fb, FileHead.bfOffBits, SEEK_SET);
-　　
-　　int len = InfoHead.biBitCount / 8;    //原图一个像素占几字节
-　　int bits_len = bits_per_pixel / 8;    //屏幕一个像素占几字节
+  //读文件信息
+  if (1 != fread( &FileHead, sizeof(BitMapFileHeader),1, fb))
+  {
+    printf("read BitMapFileHeader error!\n");
+    fclose(fb);
+    return -1;
+  }
+  if (memcmp(FileHead.bfType, "BM", 2) != 0)
+  {
+    printf("it's not a BMP file\n");
+    fclose(fb);
+    return -1;
+  }
+  
+  //读位图信息
+  if (1 != fread( (char *)&InfoHead, sizeof(BitMapInfoHeader),1, fb))
+  {
+    printf("read BitMapInfoHeader error!\n");
+    fclose(fb);
+    return -1;
+  }
+  
+  //跳转至数据区
+  fseek(fb, FileHead.bfOffBits, SEEK_SET);
+  
+  int len = InfoHead.biBitCount / 8;    //原图一个像素占几字节
+  int bits_len = bits_per_pixel / 8;    //屏幕一个像素占几字节
 
 //循环显示
-　　while(!feof(fb))
-　　{
-　　　　tmp = 0;
-　　　　rgb.Reserved = 0xFF;
-　　
-　　　　if (len != fread((char *)&rgb, 1, len, fb))
-　　　　　　break;
-　　
-　　　　//计算该像素在映射内存起始地址的偏移量
-　　　　location = line_x * bits_len + (InfoHead.biHeight - line_y - 1) * xres * bits_len;
-　　
-　　　　tmp |= rgb.Reserved << 24 | rgb.Red << 16 | rgb.Green << 8 | rgb.Blue;
-　　
-　　　　*((unsigned long *)(fbp + location)) = tmp;    
-　　
-　　　　line_x++;    
-　　　　if (line_x == InfoHead.biWidth )
-　　　　{
-　　　　　　line_x = 0;
-　　　　　　line_y++;
-　　　　　　if(line_y == InfoHead.biHeight)    
-　　　　　　　　break;    
-　　　　}    
-　　}
-　　
-　　fclose(fb);
+  while(!feof(fb))
+  {
+    tmp = 0;
+    rgb.Reserved = 0xFF;
+  
+    if (len != fread((char *)&rgb, 1, len, fb))
+      break;
+  
+    //计算该像素在映射内存起始地址的偏移量
+    location = line_x * bits_len + (InfoHead.biHeight - line_y - 1) * xres * bits_len;
+  
+    tmp |= rgb.Reserved << 24 | rgb.Red << 16 | rgb.Green << 8 | rgb.Blue;
+  
+    *((unsigned long *)(fbp + location)) = tmp;    
+  
+    line_x++;    
+    if (line_x == InfoHead.biWidth )
+    {
+      line_x = 0;
+      line_y++;
+      if(line_y == InfoHead.biHeight)    
+        break;    
+    }    
+  }
+  
+  fclose(fb);
 
-　　return 0;
+  return 0;
 }
