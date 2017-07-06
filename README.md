6313cdb52e7747466d36a12bf684d43311cde0a1
shengling <shenglinzh@gmail.com>
2017-07-06 14:44:15 +0800

    highlight bookmarks

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ linux study and app
 2007 找到了 LFS(linux from scratch) 以某个linux系统为平台，全新配置和编译一个自己定制的linux系统，从lib到core到usr，再到Xwindow。
      编译出来的 lfs 不到200M，使用了半年多。缺少包管理工具，更新就要重新编译。
      此时，ubuntu 兴起了，从此开始了ubuntu + vm的双系统之旅， 直到2013年。
-2014 在新公司使用 centos 作为服务器，至今
+2014 在新公司使用 centos 作为服务器，至今(2016-11)
 
 lubuntu VS linuxMint 为低配的旧电脑找合适的系统。
---
+++ el/hlbm.el
@@ -0,0 +1,63 @@
+;; 2017-07-06 14:24:05 zsl
+(defun highlight-bookmarks-the-buffer ()
+  (interactive)
+  (mapcar
+   (lambda (bmk)
+	 (if (string= (buffer-file-name) (bookmark-get-filename bmk))
+		 (let ((pos (bookmark-get-position bmk))
+			   )
+		   (setq hlpos (make-overlay pos (+ 6 pos)))
+		   (prin1 hlpos)
+		   (overlay-put hlpos 'face '(:background "green"))
+		   (overlay-put hlpos 'line-highlight-overlay-marker t))
+	   ;; (message bmk)
+	   ))
+   (bookmark-all-names))
+  )
+
+(defun highlight-bookmarks-clean ()
+  (interactive)
+  (remove-overlays (point-min) (point-max)))
+
+
+(defun find-overlays-specifying (prop pos)
+  (let ((overlays (overlays-at pos))
+        found)
+    (while overlays
+      (let ((overlay (car overlays)))
+        (if (overlay-get overlay prop)
+            (setq found (cons overlay found))))
+      (setq overlays (cdr overlays)))
+    found))
+
+(defun highlight-or-dehighlight-line ()
+  (interactive)
+  (if (find-overlays-specifying
+       'line-highlight-overlay-marker
+       (line-beginning-position))
+      (remove-overlays (line-beginning-position) (+ 1 (line-end-position)))
+    (let ((overlay-highlight (make-overlay
+                              (line-beginning-position)
+                              (+ 1 (line-end-position)))))
+	  (overlay-put overlay-highlight 'face '(:background "lightgreen"))
+	  (overlay-put overlay-highlight 'line-highlight-overlay-marker t))))
+
+
+(global-set-key [f8] 'highlight-or-dehighlight-line)
+
+;; (Here find-overlays-specifying came from the manual page)
+;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Finding-Overlays.html
+
+;; if you need to change the color, this is the list 
+;; http://raebear.net/comp/emacscolors.html
+
+;; It will highlight current line, and when used again it will remove it.
+
+;; Maybe the following could be useful as well: removing all your highlight from the buffer (could be dangerous, you might not want it if you highlight important things)
+
+(defun remove-all-highlight ()
+  (interactive)
+  (remove-overlays (point-min) (point-max))
+  )
+
+(global-set-key [f9] 'remove-all-highlight)
